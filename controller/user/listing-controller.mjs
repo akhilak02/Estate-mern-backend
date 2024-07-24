a346fd63d692d3ea25dc8a25dac16c3f98014c87
akhilak02 <vkdakhila@gmail.com>
2024-07-24 11:41:38 +0530

    complete updatelisting api

--- controller/user/listing-controller.mjs
+++ controller/user/listing-controller.mjs
@@ -1,9 +1,7 @@
 import Listing from "../../models/listing-model.mjs";
 
 export const createListing=async(req,res)=>{
-    try {
-      
-          
+    try {      
     const listing = await Listing.create(req.body);
    
     res.json({success:true,listing})
@@ -12,4 +10,33 @@ export const createListing=async(req,res)=>{
        res.json({success:false,err_msg:"internal server error"})
     }
 
+}
+
+export const deleteListing=async(req,res)=>{
+    const listing= await Listing.findById(req.params.id);
+    if(!listing){
+        return res.json({success:false,err_msg:"Listing not found"})
+    }
+    try {
+
+        await Listing.findByIdAndDelete(req.params.id)
+        res.json({success:true })
+        
+    } catch (error) {
+        
+    }
+}
+
+export const updateListing=async(req,res)=>{
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return res.json({ success: false, err_msg: "Listing not found" });
+    }
+    try {
+    const updatedListing= await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+       res.json({success:true,updatedListing}) 
+    } catch (error) {
+      console.log(error);  
+      res.json({success:false,err_msg:"internal server error"})
+    }
 }
--- routes/user/listingRoute.mjs
+++ routes/user/listingRoute.mjs
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createListing } from "../../controller/user/listing-controller.mjs";
+import { createListing, deleteListing, updateListing } from "../../controller/user/listing-controller.mjs";
 import { verifyUser } from "../../middleware/auth.mjs";
 
 
 
 const route= Router()
 route.post('/create',verifyUser,createListing)
-
+route.delete('/delete/:id',verifyUser,deleteListing)
+route.post('/update/:id',verifyUser,updateListing)
 export default route
